fdb6b337011d7290f8dafefc4f889511105a68ee
minokki <regular2644@gmail.com>
2023-07-07 11:14:21 +0900

    feat : 로그인,로그아웃 테스트

--- src/main/java/com/grassshop/account/AccountController.java
+++ src/main/java/com/grassshop/account/AccountController.java
@@ -22,19 +22,19 @@ public class AccountController {
     private final AccountService accountService;
     private final AccountRepository accountRepository;
 
-
+    //유효성 검증
     @InitBinder("signUpForm") //해당 모델에 초기화 바인더 설정
     public void initBinder(WebDataBinder webDataBinder){
         webDataBinder.addValidators(signUpFormValidator);
        // addValidators() 메소드를 사용하여 유효성 검증기를 등록하면, 해당 폼 데이터의 유효성 검증을 수행할 수 있음
     }
-
+    //회원가입 get
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute("signUpForm",new SignUpForm());
         return "account/sign-up";
     }
-
+    //회원가입 post
     @PostMapping("/sign-up")
     public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){
         if(errors.hasErrors()){
@@ -45,7 +45,7 @@ public class AccountController {
         //todo 회원가입 처리
         return "redirect:/";
     }
-
+    //이메일 토큰
     @GetMapping("/check-email-token")
     public String CheckEmailToken(String token, String email, Model model){
         Account account = accountRepository.findByEmail(email);
@@ -65,12 +65,13 @@ public class AccountController {
         return view;
 
     }
+    //인증메일
     @GetMapping("/check-email")
     public String checkEmail(@CurrentUser Account account, Model model){
         model.addAttribute("email",account.getEmail());
         return "account/check-email";
     }
-
+    //인증메일 재전송
     @GetMapping("resend-confirm-email")
     public String resendConfirmEmail(@CurrentUser Account account, Model model){
         if(!account.canSendConfirmEmail()){
